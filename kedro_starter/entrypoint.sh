ae20a0aa66ede46cfccdef1cff0a658fa7c61f41
Simon-Bumm <Simon_Bumm@mckinsey.com>
2021-03-25 11:24:57 +0100

    made the lookup of the kedro directory in entrypoint.sh more robust

--- kedro_starter/entrypoint.sh
+++ kedro_starter/entrypoint.sh
@@ -1,4 +1,4 @@
-dir=$(find . -type d | grep -Ev "(^\.$)|(^[^\/]?\/[^\/]+\/.*$)|(^\.\/\..*$)|(^.*kedro_starter.*$)")
-dir=$(basename $dir)
+dir=$(find . -type d | grep -E "^\.\/.+\/conf/local$")
+dir=$(echo $dir | cut -d "/" -f2)
 dir=$(echo $dir | xargs)
 cd $(pwd)/$dir && echo $(pwd) && kedro jupyter lab --ip 0.0.0.0 || kedro new --config kedro_starter/config.yml && echo "please restart the docker container"
